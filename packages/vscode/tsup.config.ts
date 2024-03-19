3409a16f0598cca2c07d6adf504f896c304208c7
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-03-20 00:01:09 +0100

    fix: vscode build needs aliasing

--- packages/vscode/tsup.config.ts
+++ packages/vscode/tsup.config.ts
@@ -1,4 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'tsup'
+
+const aliases = {
+  '@vue/compiler-sfc': '@vue/compiler-sfc/dist/compiler-sfc.esm-browser.js',
+  lightningcss: 'lightningcss-wasm/index.mjs',
+}
+
+const nodeModulesPath = path.resolve(__dirname, 'node_modules')
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/server.ts'],
@@ -8,4 +16,21 @@ export default defineConfig({
   outDir: 'dist',
   clean: true,
   shims: true,
+  esbuildPlugins: [
+    {
+      name: 'resolve-alias-plugin',
+      setup(build) {
+        build.onResolve({ filter: /.*/ }, (args) => {
+          for (const alias in aliases) {
+            if (args.path.startsWith(alias)) {
+              const updated = path.resolve(nodeModulesPath, aliases[alias])
+
+              return { path: updated }
+            }
+          }
+          return null
+        })
+      },
+    },
+  ],
 })
